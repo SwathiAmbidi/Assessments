2ec7b10223415ebce97e0df5f1f1529e48f5e0a8
SwathiAmbidi <82094551+SwathiAmbidi@users.noreply.github.com>
2021-05-21 16:42:08 +0530

    Add files via upload

---
+++ onlineplantnursery/planter/entity/Planter.java
@@ -0,0 +1,17 @@
+package com.ec.onlineplantnursery.planter.entity;
+
+import com.ec.onlineplantnursery.plant.entity.Plant;
+import com.ec.onlineplantnursery.seed.entity.Seed;
+
+public class Planter {
+	private Integer planterId;
+	private float planterheight;
+	private int planterCapacity;
+	private int drinageHoles;
+	private int planterColor;
+	private String planterShape;
+	private int planterStock;
+	private int planterCost;
+	private Plant plants;
+	private Seed seeds;
+}
